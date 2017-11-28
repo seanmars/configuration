a0121d402f4de3ef86bab4bd81e414613c8eef99
Mars <mars0222@gmail.com>
2017-11-28 22:44:26 +0800

    Update alias.sh

--- osx/scripts/alias.sh
+++ osx/scripts/alias.sh
@@ -2,9 +2,6 @@
 
 echo "Load ${0##*/}"
 
-# source tree
-alias stree='/Applications/SourceTree.app/Contents/Resources/stree'
-
 # ls use zsh+oh my zsh instead
 # alias la="ls -a"
 # alias ll="ls -l"
