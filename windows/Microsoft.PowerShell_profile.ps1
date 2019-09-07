39548cabb7ace995252aa13fdf03d7e4c6d5398e
Mars <mars0222@gmail.com>
2019-09-07 18:20:58 +0800

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,16 @@
+<# Setting #>
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+<# Alias #>
 New-Alias ll Get-ChildItem
 New-Alias which Get-Command
 
-<# Simulate the touch command in Unix-like operating systems #>
+<# Functions #>
+# Open the file explorer on current path
+function exp { Invoke-Item . }
+
+# Simulate the touch command in Unix-like operating systems
 function touch { if((Test-Path -Path ($args[0])) -eq $false) { set-content -Path ($args[0]) -Value ($null) } }
 
-<# Update all git submodules #>
+# Git command: Update all git submodules
 function smp { git submodule foreach git pull }
