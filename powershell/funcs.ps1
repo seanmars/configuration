ad3c2a8dcd890e96204f43c4fbb255aba97e3eb2
Mars <mars0222@gmail.com>
2019-09-11 17:12:36 +0800

    Create funcs.ps1

---
+++ powershell/funcs.ps1
@@ -0,0 +1,15 @@
+# PAUSE "Press any key to continue..."
+Function PAUSE ($message)
+{
+    # Check if running Powershell ISE
+    if ($psISE)
+    {
+        Add-Type -AssemblyName System.Windows.Forms
+        [System.Windows.Forms.MessageBox]::Show("$message")
+    }
+    else
+    {
+        Write-Host "$message" -ForegroundColor Yellow
+        $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    }
+}
