ebeafad7c82699930e704ff908231e74705672e4
Mars <mars0222@gmail.com>
2020-02-12 13:45:46 +0800

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,18 @@
 <# %UserProfile%/Documents/PowerShell/ #>
 
+Import-Module Get-ChildItemColor
+
+$env:PYTHONIOENCODING="utf-8"
+
+# Remove-Item alias:ls -force
+Set-Alias l Get-ChildItemColor -option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+
+<# oh-my-posh #>
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Mars
+
 <# Setting #>
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
@@ -14,9 +27,6 @@ function exp { Invoke-Item . }
 # Simulate the touch command in Unix-like operating systems
 function touch { if((Test-Path -Path ($args[0])) -eq $false) { set-content -Path ($args[0]) -Value ($null) } }
 
-# Git command: Update all git submodules
-function smp { git submodule foreach git pull }
-
 # Generate Guid
 function guid
 {
@@ -24,3 +34,7 @@ function guid
   $output.Guid
 }
 New-Alias uuid guid
+
+<# Git command #>
+# Update all git submodules
+function gsmp { git submodule foreach git pull }
