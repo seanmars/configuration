edea25daab4efd7d53a0363a05e1cdef1a466c3c
Mars <mars0222@gmail.com>
2020-02-15 14:41:49 +0800

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,6 @@
-<# %UserProfile%/Documents/PowerShell/ #>
-
 Import-Module Get-ChildItemColor
 
-$env:PYTHONIOENCODING="utf-8"
+$env:PYTHONIOENCODING = "utf-8"
 
 # Remove-Item alias:ls -force
 Set-Alias l Get-ChildItemColor -option AllScope
@@ -14,27 +12,30 @@ Import-Module oh-my-posh
 Set-Theme Mars
 
 <# Setting #>
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
+# Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 <# Alias #>
-New-Alias ll Get-ChildItem
-New-Alias which Get-Command
+Set-Alias ll Get-ChildItem
+Set-Alias which Get-Command
+
+# With orang
+if (Get-Command orang -errorAction SilentlyContinue) {
+  function OrangFind {
+    orang find -y l p=f @args
+  }
+  Set-Alias ff OrangFind
+}
 
 <# Functions #>
 # Open the file explorer on current path
 function exp { Invoke-Item . }
 
-# Simulate the touch command in Unix-like operating systems
-function touch { if((Test-Path -Path ($args[0])) -eq $false) { set-content -Path ($args[0]) -Value ($null) } }
-
 # Generate Guid
-function guid
-{
+function guid {
   $output = New-Guid
   $output.Guid
 }
-New-Alias uuid guid
+Set-Alias uuid guid
 
-<# Git command #>
-# Update all git submodules
+<# Update all git submodules #>
 function gsmp { git submodule foreach git pull }
